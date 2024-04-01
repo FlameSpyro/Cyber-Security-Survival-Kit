8b3362ee10c0a3e3c9ab731fbead7af1706daeab
FlameSpyro <70916199+FlameSpyro@users.noreply.github.com>
2024-04-01 19:15:27 -0400

    Update Insta_Choco.ps1
    After process runs then choco is installed.

--- Code/ChocoInstalls/Insta_Choco.ps1
+++ Code/ChocoInstalls/Insta_Choco.ps1
@@ -13,3 +13,6 @@ while ($true) {
         break
     }
 }
+
+# Install Chocolatey (Will require restart after installation)
+Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))
