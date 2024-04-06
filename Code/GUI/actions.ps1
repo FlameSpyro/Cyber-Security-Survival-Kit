50587bbe71409d7560b47945f17887c16708b62b
FlameSpyro <70916199+FlameSpyro@users.noreply.github.com>
2024-04-06 16:27:29 -0400

    Update actions.ps1
    Proper switch structure added

--- Code/GUI/actions.ps1
+++ Code/GUI/actions.ps1
@@ -1,1 +1,39 @@
-# Will be filled in later
+param (
+    [switch]$a,
+    [switch]$b,
+    [switch]$c,
+    [switch]$d,
+    [switch]$e,
+    [switch]$f
+)
+
+if ($a) {
+    Write-Output "Command for flag -a"
+    
+}
+
+if ($b) {
+    Write-Output "Command for flag -b"
+    
+}
+
+if ($c) {
+    Write-Output "Command for flag -c"
+    
+}
+
+if ($d) {
+    Write-Output "Command for flag -d"
+    
+}
+
+if ($e) {
+    Write-Output "Command for flag -e"
+    
+}
+
+if ($f) {
+    Write-Output "Command for flag -f"
+    
+}
+
