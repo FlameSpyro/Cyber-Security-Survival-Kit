ec32fdfe2ffeacd5202c2e3cb75c9dcf9bd87e79
FlameSpyro <70916199+FlameSpyro@users.noreply.github.com>
2024-04-06 17:12:44 -0400

    Update actions.ps1

--- Code/GUI/actions.ps1
+++ Code/GUI/actions.ps1
@@ -9,26 +9,56 @@ param (
 
 if ($a) {
     Write-Output "Command for flag -a"
-    
+    ping 8.8.8.8
 }
 
 if ($b) {
     Write-Output "Command for flag -b"
-    
+    ping champlain.edu
 }
 
 if ($c) {
     Write-Output "Command for flag -c"
-    
+    ping 8.8.8.8
 }
 
 if ($d) {
     Write-Output "Command for flag -d"
-    
+    Start-Process "ms-settings:windowsdefender"
 }
 
 if ($e) {
     Write-Output "Command for flag -e"
+    
+}
+
+if ($f) {
+    Write-Output "Command for flag -f"
+    
+}
+
+if ($g) {
+    Write-Output "Command for flag -b"
+    ping champlain.edu
+}
+
+if ($h) {
+    Write-Output "Command for flag -c"
+    ping 8.8.8.8
+}
+
+if ($i) {
+    Write-Output "Command for flag -d"
+    Start-Process "ms-settings:windowsdefender"
+}
+
+if ($j) {
+    Write-Output "Command for flag -e"
+    
+}
+
+if ($k) {
+    Write-Output "Command for flag -f"
     
 }
 
